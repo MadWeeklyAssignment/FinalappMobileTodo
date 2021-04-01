87616880c2c3d4f42d5b448664fbcfdb3fb59b35
AnantaNeupane <neupaneananta18@gmail.com>
2021-04-01 22:34:45 +0545

    setting up recycle view in mainactivity

--- FinalappMobileTodo/app/src/main/java/com/example/finalappmobiletodo/MainActivity.java
+++ FinalappMobileTodo/app/src/main/java/com/example/finalappmobiletodo/MainActivity.java
@@ -1,20 +1,46 @@
 package com.example.finalappmobiletodo;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.finalappmobiletodo.data.Repository;
+import com.example.finalappmobiletodo.data.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int NEW_WORD_ACTIVITY_REQUEST_CODE = 1;
+    public static final int UPDATE_WORD_ACTIVITY_REQUEST_CODE = 2;
+
+    public static final String EXTRA_DATA_UPDATE_WORD = "extra_word_to_be_updated";
+    public static final String EXTRA_DATA_ID = "extra_data_id";
+
+    private MainViewModel mMainViewModel;
+    private boolean isFragmentDisplayed = false;
+    static final String STATE_FRAGMENT = "state_of_fragment";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
-    // Set up the RecyclerView.
-    RecyclerView recyclerView = findViewById(R.id.recyclerview);
-    final TaskAdapter adapter = new TaskAdapter(this);
+        if (savedInstanceState != null) {
+            isFragmentDisplayed = savedInstanceState.getBoolean(STATE_FRAGMENT);
+        }
+        // Set up the RecyclerView.
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        final TaskAdapter adapter = new TaskAdapter(this);
         recyclerView.setAdapter(adapter);
-                recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
