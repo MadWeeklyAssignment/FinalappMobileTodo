cae571226bc9dcc83562c749811fb12f7df4fae6
AnantaNeupane <neupaneananta18@gmail.com>
2021-04-01 17:44:48 +0545

    entity class that represent word in database created

--- FinalappMobileTodo/app/src/main/java/com/example/finalappmobiletodo/data/task.java
+++ FinalappMobileTodo/app/src/main/java/com/example/finalappmobiletodo/data/task.java
@@ -2,3 +2,27 @@ package com.example.finalappmobiletodo.data;
 
 public class task {
 }
+import androidx.annotation.NonNull;
+        import androidx.room.ColumnInfo;
+        import androidx.room.Entity;
+        import androidx.room.Ignore;
+        import androidx.room.PrimaryKey;
+
+/**
+ * Entity class that represents a word in the database
+ */
+
+@Entity(tableName = "word_table")
+public class task {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @NonNull
+    @ColumnInfo(name = "word")
+    private String mWord;
+
+    public task(@NonNull String word) {
+        this.mWord = word;
+    }
+
