9c0994bf555f987cdf21e947db3d6be349b07242
AnantaNeupane <neupaneananta18@gmail.com>
2021-04-01 20:09:10 +0545

    populate the database

--- FinalappMobileTodo/app/src/main/java/com/example/finalappmobiletodo/data/AppDatabase.java
+++ FinalappMobileTodo/app/src/main/java/com/example/finalappmobiletodo/data/AppDatabase.java
@@ -41,3 +41,29 @@ public abstract class AppDatabase extends RoomDatabase {
                     new PopulateDbAsync(INSTANCE).execute();
                 }
             };
+    private static class PopulateDbAsync extends AsyncTask<Void, Void, Void> {
+
+        private final TodoDao mDao;
+
+        // Initial data set
+        private static String [] words = {"dolphin", "crocodile", "cobra", "elephant", "goldfish",
+                "tiger", "snake"};
+
+        PopulateDbAsync(AppDatabase db) {
+            mDao = db.wordDao();
+        }
+
+        @Override
+        protected Void doInBackground(final Void... params) {
+            // If we have no words, then create the initial list of words.
+            if (mDao.getAnyWord().length < 1) {
+                for (int i = 0; i <= words.length - 1; i++) {
+                    Task task = new Task(words[i]);
+                    mDao.insert(task);
+                }
+            }
+            return null;
+        }
+    }
+}
+
