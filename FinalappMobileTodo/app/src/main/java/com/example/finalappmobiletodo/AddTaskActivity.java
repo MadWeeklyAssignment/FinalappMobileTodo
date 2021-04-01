e0e4dd7e3566625b8c702059fed477a43915596b
AnantaNeupane <neupaneananta18@gmail.com>
2021-04-01 18:54:14 +0545

    importing packages for addtaskactivity

--- FinalappMobileTodo/app/src/main/java/com/example/finalappmobiletodo/AddTaskActivity.java
+++ FinalappMobileTodo/app/src/main/java/com/example/finalappmobiletodo/AddTaskActivity.java
@@ -1,4 +1,16 @@
 package com.example.finalappmobiletodo;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import static com.example.finalappmobiletodo.MainActivity.EXTRA_DATA_ID;
+import static com.example.finalappmobiletodo.MainActivity.EXTRA_DATA_UPDATE_WORD;
 public class AddTaskActivity {
 }
