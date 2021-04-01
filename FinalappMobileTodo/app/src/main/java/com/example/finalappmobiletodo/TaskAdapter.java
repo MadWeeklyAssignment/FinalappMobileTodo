bccd916bccefafbd678c881ef183cd65c3123f1e
AnantaNeupane <neupaneananta18@gmail.com>
2021-04-01 19:09:56 +0545

    adapter for recycler view displaying list of word added

--- FinalappMobileTodo/app/src/main/java/com/example/finalappmobiletodo/TaskAdapter.java
+++ FinalappMobileTodo/app/src/main/java/com/example/finalappmobiletodo/TaskAdapter.java
@@ -1,4 +1,19 @@
 package com.example.finalappmobiletodo;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.finalappmobiletodo.data.Task;
 
-public class TaskAdapter {
-}
+import java.util.List;
+public class TaskAdapter extends RecyclerView.Adapter<TaskAdapter.WordViewHolder> {
+
+    private final LayoutInflater mInflater;
+    private List<Task> mTasks; // Cached copy of words
+    private static ClickListener clickListener;
+
+    TaskAdapter(Context context) {
+        mInflater = LayoutInflater.from(context);
+    }
