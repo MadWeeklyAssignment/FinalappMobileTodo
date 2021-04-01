1dee32a0dfbc68ff6125c36089fe1b55ba39bc38
AnantaNeupane <neupaneananta18@gmail.com>
2021-04-01 22:51:41 +0545

    wordviewmodel class created showing interface between ui and data layer

--- FinalappMobileTodo/app/src/main/java/com/example/finalappmobiletodo/MainViewModel.java
+++ FinalappMobileTodo/app/src/main/java/com/example/finalappmobiletodo/MainViewModel.java
@@ -1,4 +1,20 @@
 package com.example.finalappmobiletodo;
 
-public class MainViewModel {
-}
+import android.app.Application;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import com.example.finalappmobiletodo.data.Repository;
+import com.example.finalappmobiletodo.data.Task;
+import java.util.List;
+
+public class MainViewModel extends AndroidViewModel {
+
+    private Repository mRepository;
+
+    private LiveData<List<Task>> mAllWords;
+
+    public MainViewModel(Application application) {
+        super(application);
+        mRepository = new Repository(application);
+        mAllWords = mRepository.getAllWords();
+    }
