5fb076dc576818af4b16e53ac60dcc711bd581fb
AnantaNeupane <neupaneananta18@gmail.com>
2021-04-01 20:07:38 +0545

    word room database and codes to create database

--- FinalappMobileTodo/app/src/main/java/com/example/finalappmobiletodo/data/AppDatabase.java
+++ FinalappMobileTodo/app/src/main/java/com/example/finalappmobiletodo/data/AppDatabase.java
@@ -1,4 +1,34 @@
 package com.example.finalappmobiletodo.data;
 
-public class AppDatabase {
-}
+import android.content.Context;
+import android.os.AsyncTask;
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+@Database(entities = {Task.class}, version = 2, exportSchema = false)
+public abstract class AppDatabase extends RoomDatabase {
+
+    public abstract TodoDao wordDao();
+
+    private static AppDatabase INSTANCE;
+
+    public static AppDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (AppDatabase.class) {
+                if (INSTANCE == null) {
+                    // Create database here.
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            AppDatabase.class, "todo.db")
+                            // Wipes and rebuilds instead of migrating if no Migration object.
+                            // Migration is not part of this practical.
+                            .fallbackToDestructiveMigration()
+                            .addCallback(sRoomDatabaseCallback)
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
