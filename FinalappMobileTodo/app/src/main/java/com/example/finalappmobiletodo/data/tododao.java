60b4030fc674486a683e4706bac3a75683b05f10
AnantaNeupane <neupaneananta18@gmail.com>
2021-04-01 17:57:18 +0545

    insert annotation added

--- FinalappMobileTodo/app/src/main/java/com/example/finalappmobiletodo/data/tododao.java
+++ FinalappMobileTodo/app/src/main/java/com/example/finalappmobiletodo/data/tododao.java
@@ -1,4 +1,20 @@
 package com.example.finalappmobiletodo.data;
 
-public class tododao {
+import androidx.lifecycle.LiveData;
+import androidx.room.*;
+
+
+import java.util.List;
+
+/**
+ * Data Access Object (DAO) for a word.
+ * Each method performs a database operation, such as inserting or deleting a word,
+ * running a DB query, or deleting all words.
+ */
+
+@Dao
+public interface tododao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(task task);
 }
